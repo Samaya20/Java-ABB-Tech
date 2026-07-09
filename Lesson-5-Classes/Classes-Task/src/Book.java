43cfcc7ddd80dc088b6ea7b1d4e55893403b241e
Samaya <muqumovas207@gmail.com>
2026-07-09 12:33:01 +0400

    Added classes task for Lesson 5

---
+++ Lesson-5-Classes/Classes-Task/src/Book.java
@@ -0,0 +1,55 @@
+public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    protected int totalCopies;
+    protected int availableCopies;
+
+    public Book(String title1, String author1, String isbn1) {
+        title = title1;
+        author = author1;
+        isbn = isbn1;
+        totalCopies = 1;
+        availableCopies = 1;
+    }
+
+    {
+        System.out.println("New book created");
+    }
+
+    public void borrowBook() {
+        if (availableCopies > 0) {
+            availableCopies--;
+            System.out.println("Book borrowed successfully.");
+        } else {
+            System.out.println("Sorry, no copies available.");
+        }
+    }
+
+    public void returnBook() {
+        if (availableCopies < totalCopies) {
+            availableCopies++;
+            System.out.println("Book returned successfully.");
+        } else {
+            System.out.println("All copies are already in the library.");
+        }
+    }
+
+    public void printInfo() {
+        System.out.println("-- Book info --");
+        System.out.println("Title : " + this.title);
+        System.out.println("Author : " + this.author);
+        System.out.println("ISBN : " + this.isbn);
+        System.out.println("Total Copies : " + this.totalCopies);
+        System.out.println("Available Copies : " + this.availableCopies);
+        System.out.println("---------");
+    }
+
+    public static void libraryRules() {
+        System.out.println("Max 3 books can be borrowed per person.");
+    }
+
+    public final void bookType() {
+        System.out.println("This is a regular book.");
+    }
+}
